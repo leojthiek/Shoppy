69d45fd59b3732c96c2807bdef6738c770b362a7
leojthiek <leojthiek@gmail.com>
2023-05-12 23:56:54 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -286,18 +286,20 @@ app.get("/api/auth/user", (req, res) => {
 })
 
 
-app.use("/api/products", productRoute)
-app.use("/users", UserRoute)
-app.use("/orders", OrderRoute)
-app.use("/api/cart", CartRoutes)
-app.use("/api", CouponRoute)
-app.use("/api", OfferRoutes)
 
 
 
 const __dirname = path.resolve()
 if(process.env.NODE_ENV === 'production'){
   app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+
+app.use("/api/products", productRoute)
+app.use("/users", UserRoute)
+app.use("/orders", OrderRoute)
+app.use("/api/cart", CartRoutes)
+app.use("/api", CouponRoute)
+app.use("/api", OfferRoutes)
 
   app.get('*',(req,res)=>
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
