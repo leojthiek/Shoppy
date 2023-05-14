f938365a7ca6906dc68087abbbb3ff4792694824
leojthiek <leojthiek@gmail.com>
2023-05-14 20:18:58 +0530

    footer

--- frontend/src/component/Footer.js
+++ frontend/src/component/Footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
         <Container>
             <Row>
                 <Col className='text-center py-3'>
-                    Copyright &copy; Leoj Store
+                    Copyright &copy; Leoj Store Official
                 </Col>
             </Row>
         </Container>
