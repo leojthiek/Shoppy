65d40b416d0b3c03201258c7613c510ff166feb7
leojthiek <leojthiek@gmail.com>
2023-05-18 19:15:39 +0530

    testing

--- frontend/src/component/adminSidebar.js
+++ frontend/src/component/adminSidebar.js
@@ -1,10 +1,14 @@
 import React from "react"
-import DashboardCustomizeOutlinedIcon from "@mui/icons-material/DashboardCustomizeOutlined"
-import ProductionQuantityLimitsOutlinedIcon from "@mui/icons-material/ProductionQuantityLimitsOutlined"
-import GroupOutlinedIcon from "@mui/icons-material/GroupOutlined"
-import LocalOfferOutlinedIcon from "@mui/icons-material/LocalOfferOutlined"
-import ConfirmationNumberIcon from "@mui/icons-material/ConfirmationNumber"
-import AssignmentTurnedInIcon from "@mui/icons-material/AssignmentTurnedIn"
+
+import {
+  MdDashboard,
+  MdOutlineProductionQuantityLimits,
+  MdLocalOffer,
+} from "react-icons/md"
+import { BsCartCheckFill } from "react-icons/bs"
+import { ImUser } from "react-icons/im"
+import { RiCoupon3Fill } from "react-icons/ri"
+
 import { LinkContainer } from "react-router-bootstrap"
 
 export default function AdminSidebar() {
@@ -14,8 +18,8 @@ export default function AdminSidebar() {
         <ul>
           <p className='sidebar-center-p'>Overview</p>
           <li>
-            <DashboardCustomizeOutlinedIcon className='sidebar-icon' />
-            <LinkContainer to={'/admin/dashboard'}>
+            <MdDashboard className='sidebar-icon' />
+            <LinkContainer to={"/admin/dashboard"}>
               <span>Dashboard</span>
             </LinkContainer>
           </li>
@@ -23,36 +27,33 @@ export default function AdminSidebar() {
           <p className='sidebar-center-p'>Controller</p>
 
           <li>
-            <ProductionQuantityLimitsOutlinedIcon className='sidebar-icon' />
+            <MdOutlineProductionQuantityLimits className='sidebar-icon' />
             <LinkContainer to={"/admin/productlist"}>
               <span>Products</span>
             </LinkContainer>
           </li>
           <li>
-            <AssignmentTurnedInIcon className='sidebar-icon' />
+            <BsCartCheckFill className='sidebar-icon' />
             <LinkContainer to={"/admin/orderlist"}>
-            <span>Orders</span>
-
+              <span>Orders</span>
             </LinkContainer>
           </li>
           <li>
-            <GroupOutlinedIcon className='sidebar-icon' />
-            <LinkContainer to={'/admin/userlist'}>
-            <span>Users</span>
-
+            <ImUser className='sidebar-icon' />
+            <LinkContainer to={"/admin/userlist"}>
+              <span>Users</span>
             </LinkContainer>
           </li>
           <li>
-            <LocalOfferOutlinedIcon className='sidebar-icon' />
-            <LinkContainer to={'/admin/offers'}>
-            <span>Offers</span>
-
+            <MdLocalOffer className='sidebar-icon' />
+            <LinkContainer to={"/admin/offers"}>
+              <span>Offers</span>
             </LinkContainer>
           </li>
           <li>
-            <ConfirmationNumberIcon className='sidebar-icon' />
-            <LinkContainer to={'/admin/coupons'}>
-            <span>Coupons</span>
+            <RiCoupon3Fill className='sidebar-icon' />
+            <LinkContainer to={"/admin/coupons"}>
+              <span>Coupons</span>
             </LinkContainer>
           </li>
         </ul>
